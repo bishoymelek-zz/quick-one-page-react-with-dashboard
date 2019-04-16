e0d586a7148d6afee482af343023f6f587543f22
unknown <bishoymelekwadie@gmail.com>
2019-04-16 17:12:48 +0200

    Remove sample

--- src/endUserSite/routes/home/index.js
+++
@@ -1,4 +0,0 @@
-import Home from './Home';
-import './style.scss';
-
-export default Home;
--- src/endUserSite/routes/routesList.js
+++
@@ -1,7 +0,0 @@
-import homePage from './home'
-export default (store) => { 
-    return [
-        { path: '/home', component: homePage },
-        { path: '/', component: homePage },
-    ]
-}
